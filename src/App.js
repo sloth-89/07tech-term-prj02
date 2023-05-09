f57b136e19a94c7f8bf4972702eb3539510ae11e
sloth-89 <restvoice2020@gmail.com>
2023-05-09 23:31:18 +0900

    230509

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import Header from './component/Header';
 import DayList from './component/DayList';
 import Day from './component/Day';
+import NotFound from './component/NotFound';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <DayList/>
-      <Day/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+
+        <Routes>
+          <Route path='/' element={<DayList/>}/>
+          <Route path='/day/:inputDay' element={<Day/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
